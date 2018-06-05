584e5c7ea7cd3003f7439d26d98571ee9225f6b4
Liod <26197815+LiodAir@users.noreply.github.com>
2018-06-05 22:18:06 +0800

    Create config_db.php

---
+++ config/config_db.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lioders
+ * Date: 18-6-2
+ * Time: 上午10:26
+ */
+
+
+global $table;
+
+
+$config = array(                 //数据库配置
+    "root",
+    "liod",
+    "news",
+    "3306",
+    "127.0.0.1",
+);
+
+
+$table = array(
+    "1"=>"标题",
+    "2"=>"日期",
+    "4"=>"url",
+    "5"=>"来源",
+    "6"=>"采集时间",
+);
+
+?>
